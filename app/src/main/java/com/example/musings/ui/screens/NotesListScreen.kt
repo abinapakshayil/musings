f31b97df0feec5d80da40ec4a11000576ffb3273
abinapakshayil <abinapakshayil@gmail.com>
2025-10-09 12:37:02 +0530

    Handle empty note list

--- app/src/main/java/com/example/musings/ui/screens/NotesListScreen.kt
+++ app/src/main/java/com/example/musings/ui/screens/NotesListScreen.kt
@@ -1,16 +1,22 @@
 package com.example.musings.ui.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -29,24 +35,40 @@ fun NotesListScreen(viewModel: NoteViewModel, navController: NavController, modi
     // Actual list
     val notesList = notes.value
 
-    var noteToDelete by remember { mutableStateOf<com.example.musings.data.local.Note?>(null) }
+    var noteToDelete by remember { mutableStateOf<Note?>(null) }
 
-
-    LazyColumn(
-        modifier = modifier
-            .padding(horizontal = 8.dp)
-    ) {
-        items(count = notesList.size) { index ->
-            val note = notesList[index]
-            NoteCard(
-                note = note,
-                onEditClick = { navController.navigate(
-                    EditNote.createRoute(note.id, note.title, note.content)
-                ) },
-                onDeleteClick = { noteToDelete = note }
+    // No notes
+    if (notesList.isEmpty()) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "No notes yet.",
+                style = MaterialTheme.typography.bodyLarge
             )
         }
+    } else {
+        LazyColumn(
+            modifier = modifier
+                .padding(horizontal = 8.dp)
+        ) {
+            items(count = notesList.size) { index ->
+                val note = notesList[index]
+                NoteCard(
+                    note = note,
+                    onEditClick = { navController.navigate(
+                        EditNote.createRoute(note.id, note.title, note.content)
+                    ) },
+                    onDeleteClick = { noteToDelete = note }
+                )
+            }
+        }
     }
+
+
+
 
     // Show confirmation dialog if a note is selected for deletion
     noteToDelete?.let { note ->
